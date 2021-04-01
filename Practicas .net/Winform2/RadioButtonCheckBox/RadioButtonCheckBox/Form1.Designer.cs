87e7e9b4ca2c4c71a0ede985b7b2d97b524bdc85
Ferrante, Bruno <bruno.ferrante@alumnos.frgp.utn.edu.ar>
2021-04-01 18:21:43 -0300

    WinForm2
    Ej1

---
+++ Practicas .net/Winform2/RadioButtonCheckBox/RadioButtonCheckBox/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace RadioButtonCheckBox
+{
+    partial class frmRadioButtonCheckBox
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRadioButtonCheckBox));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblProgramador = new System.Windows.Forms.Label();
+            this.lblSalida = new System.Windows.Forms.Label();
+            this.txtSalida = new System.Windows.Forms.TextBox();
+            this.chkbProgramador = new System.Windows.Forms.CheckBox();
+            this.gpbSexo = new System.Windows.Forms.GroupBox();
+            this.rbFemenino = new System.Windows.Forms.RadioButton();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.gpbSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.Color.Transparent;
+            this.lblNombre.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblNombre.ForeColor = System.Drawing.Color.White;
+            this.lblNombre.Location = new System.Drawing.Point(72, 121);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(99, 26);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre: ";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNombre.Location = new System.Drawing.Point(276, 122);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(669, 16);
+            this.txtNombre.TabIndex = 1;
+            this.txtNombre.Validating += new System.ComponentModel.CancelEventHandler(this.txtNombre_Validating);
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.Transparent;
+            this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOK.ForeColor = System.Drawing.Color.White;
+            this.btnOK.Location = new System.Drawing.Point(1058, 118);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblProgramador
+            // 
+            this.lblProgramador.AutoSize = true;
+            this.lblProgramador.BackColor = System.Drawing.Color.Transparent;
+            this.lblProgramador.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblProgramador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblProgramador.ForeColor = System.Drawing.Color.White;
+            this.lblProgramador.Location = new System.Drawing.Point(72, 184);
+            this.lblProgramador.Name = "lblProgramador";
+            this.lblProgramador.Size = new System.Drawing.Size(145, 26);
+            this.lblProgramador.TabIndex = 3;
+            this.lblProgramador.Text = "Programador: ";
+            // 
+            // lblSalida
+            // 
+            this.lblSalida.AutoSize = true;
+            this.lblSalida.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalida.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSalida.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblSalida.ForeColor = System.Drawing.Color.White;
+            this.lblSalida.Location = new System.Drawing.Point(72, 401);
+            this.lblSalida.Name = "lblSalida";
+            this.lblSalida.Size = new System.Drawing.Size(81, 26);
+            this.lblSalida.TabIndex = 4;
+            this.lblSalida.Text = "Salida: ";
+            // 
+            // txtSalida
+            // 
+            this.txtSalida.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtSalida.Location = new System.Drawing.Point(72, 436);
+            this.txtSalida.Multiline = true;
+            this.txtSalida.Name = "txtSalida";
+            this.txtSalida.Size = new System.Drawing.Size(1061, 139);
+            this.txtSalida.TabIndex = 5;
+            // 
+            // chkbProgramador
+            // 
+            this.chkbProgramador.AutoSize = true;
+            this.chkbProgramador.BackColor = System.Drawing.Color.Transparent;
+            this.chkbProgramador.Location = new System.Drawing.Point(276, 192);
+            this.chkbProgramador.Name = "chkbProgramador";
+            this.chkbProgramador.Size = new System.Drawing.Size(15, 14);
+            this.chkbProgramador.TabIndex = 6;
+            this.chkbProgramador.UseVisualStyleBackColor = false;
+            // 
+            // gpbSexo
+            // 
+            this.gpbSexo.BackColor = System.Drawing.Color.Transparent;
+            this.gpbSexo.CausesValidation = false;
+            this.gpbSexo.Controls.Add(this.rbFemenino);
+            this.gpbSexo.Controls.Add(this.rbMasculino);
+            this.gpbSexo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gpbSexo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.gpbSexo.ForeColor = System.Drawing.Color.White;
+            this.gpbSexo.Location = new System.Drawing.Point(72, 248);
+            this.gpbSexo.Name = "gpbSexo";
+            this.gpbSexo.Size = new System.Drawing.Size(1061, 121);
+            this.gpbSexo.TabIndex = 7;
+            this.gpbSexo.TabStop = false;
+            this.gpbSexo.Text = "Sexo";
+            this.gpbSexo.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // rbFemenino
+            // 
+            this.rbFemenino.AutoSize = true;
+            this.rbFemenino.Location = new System.Drawing.Point(760, 63);
+            this.rbFemenino.Name = "rbFemenino";
+            this.rbFemenino.Size = new System.Drawing.Size(113, 29);
+            this.rbFemenino.TabIndex = 1;
+            this.rbFemenino.TabStop = true;
+            this.rbFemenino.Text = "Femenino";
+            this.rbFemenino.UseVisualStyleBackColor = true;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(204, 63);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(117, 29);
+            this.rbMasculino.TabIndex = 0;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // frmRadioButtonCheckBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1184, 636);
+            this.Controls.Add(this.gpbSexo);
+            this.Controls.Add(this.chkbProgramador);
+            this.Controls.Add(this.txtSalida);
+            this.Controls.Add(this.lblSalida);
+            this.Controls.Add(this.lblProgramador);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmRadioButtonCheckBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Radio Button y CheckBox";
+            this.TransparencyKey = System.Drawing.Color.DarkSlateGray;
+            this.Load += new System.EventHandler(this.frmRadioButtonCheckBox_Load);
+            this.gpbSexo.ResumeLayout(false);
+            this.gpbSexo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblProgramador;
+        private System.Windows.Forms.Label lblSalida;
+        private System.Windows.Forms.TextBox txtSalida;
+        private System.Windows.Forms.CheckBox chkbProgramador;
+        private System.Windows.Forms.GroupBox gpbSexo;
+        private System.Windows.Forms.RadioButton rbFemenino;
+        private System.Windows.Forms.RadioButton rbMasculino;
+    }
+}
+
---
+++ Practicas .net/Winform2/RadioButtonCheckBox/RadioButtonCheckBox/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadioButtonCheckBox
+{
+    public partial class frmRadioButtonCheckBox : Form
+    {
+        private void ValidarOK()
+        {
+            btnOK.Enabled = (txtNombre.BackColor != Color.Red);
+        }
+        public frmRadioButtonCheckBox()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmRadioButtonCheckBox_Load(object sender, EventArgs e)
+        {
+            //Deshabilito botoncin
+            btnOK.Enabled = false;
+        }
+
+        private void txtNombre_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+
+            if(tb.TextLength == 0)
+            {
+                tb.BackColor = Color.Red;
+            }
+            else
+            {
+                tb.BackColor = System.Drawing.SystemColors.Window;
+
+            }
+            ValidarOK();
+
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            String salida;
+
+            salida = "Nombre: " + txtNombre.Text + "\r\n";
+            salida += "ocupación: " + (string)(chkbProgramador.Checked ?
+            "Programador " : "No es programador") + "\r\n";
+
+            salida += "Sexo: " + (string)(rbFemenino.Checked ? "Mujer" : "Hombre") + "\r\n";
+
+            txtSalida.Text = salida;
+        }
+    }
+}
